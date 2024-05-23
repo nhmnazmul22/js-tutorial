34f1ef24afbf5c8b481359bddd162e5a991f8852
Nazmul Hasan <nhmnazmul87@gmail.com>
2024-05-23 20:22:50 +0600

    Js Object && Array Basic Completed

---
+++ basic js/script.js
@@ -0,0 +1,17 @@
+/*============= Js Object && Array  ================ */
+
+const obj = {
+  fristName: "Nazmul",
+  middleName: "Hasan",
+  lastName: "Mamun",
+  address: "west box nagar, sarulia, demra, Dhaka",
+  country: "Bangladesh",
+};
+
+const arry = ["apple", "bannana", "mango"];
+
+const myDetails = `My name is ${obj.fristName} ${obj.middleName} ${obj.lastName} and I live in ${obj.address}`;
+
+console.log(obj);
+console.log(arry);
+console.log(myDetails);
