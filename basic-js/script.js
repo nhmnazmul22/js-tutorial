d6946b3a7366a8a1589516fe444dda3802838c34
Nazmul Hasan <nhmnazmul87@gmail.com>
2024-05-24 11:10:09 +0600

    If/if else/if else if && Switch statement

--- basic-js/script.js
+++ basic-js/script.js
@@ -17,10 +17,48 @@ console.log(arry);
 console.log(myDetails);
 /*============= If, if else, if else if  ================ */
 
-const age = 18;
+let mark = 85;
 
-if (age > 18) {
-  console.log("You are Adult");
+if (mark >= 80 && mark <= 100) {
+  console.log("You got A+");
+} else if (mark >= 70 && mark < 80) {
+  console.log("You got A");
+} else if (mark >= 60 && mark < 50) {
+  console.log("You got A-");
+} else if (mark >= 50 && mark < 60) {
+  console.log("You got B");
+} else if (mark >= 40 && mark < 50) {
+  console.log("You got C");
+} else if (mark >= 33 && mark < 40) {
+  console.log("You got D");
 } else {
-  console.log("You are Child");
+  console.log("Do better Next time! You got F");
 }
+
+/*============= Switch Statement  ================ */
+
+let mark2 = 67;
+
+switch (true) {
+  case mark2 >= 80 && mark2 <= 100:
+    console.log("You got A+");
+    break;
+  case mark2 >= 70 && mark2 < 80:
+    console.log("You got A");
+    break;
+  case mark2 >= 60 && mark2 < 70:
+    console.log("You got A-");
+    break;
+  case mark2 >= 50 && mark2 < 60:
+    console.log("You got B");
+    break;
+  case mark2 >= 40 && mark2 < 50:
+    console.log("You got C");
+    break;
+  case mark2 >= 33 && mark2 < 40:
+    console.log("You got D");
+    break;
+  default:
+    console.log("Do better Next time! You got F");
+}
+/*============= Basic For loop  ================ */
